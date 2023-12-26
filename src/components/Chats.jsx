90680da8fdb8f2e5fffbc584e5f28542f48f4691
doomdybbuk <manan.jain16949@gmail.com>
2023-12-26 23:49:04 +0530

    Created Messages component and gave some major styling

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,7 +2,29 @@ import React from "react";
 
 const Chats = () =>{
     return(
-        <div>Chats</div>
+        <div className="chats">
+            <div className="userChat">
+                <img src="https://i.ibb.co/cLdxrjc/portfolio-Image.jpg" alt="" />
+                <div className="userChatInfo">
+                    <span>Jane</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="userChat">
+                <img src="https://i.ibb.co/cLdxrjc/portfolio-Image.jpg" alt="" />
+                <div className="userChatInfo">
+                    <span>Jane</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="userChat">
+                <img src="https://i.ibb.co/cLdxrjc/portfolio-Image.jpg" alt="" />
+                <div className="userChatInfo">
+                    <span>Jane</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
