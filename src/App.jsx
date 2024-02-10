47a81b7710fe15dbc4da1629bd76e47a7d234439
doomdybbuk <manan.jain16949@gmail.com>
2024-02-11 01:25:17 +0530

    Handling authentication

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import "./style.scss";
 
 function App(){
   return(
-    <Home/>
-    // <Register/>
+    // <Home/>
+    <Register/>
     // <Login/>
   )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Add from "../img/addAvatar.png"
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import {auth} from "../firebase"
 
 const Register = () =>{
+    const [err,setErr] = useState(false)
+    // This function will check for the input 0-> Name , 1-> Email id 2-> Password and 3 -> The avatar. This is useful for authentication
+    const handleSubmit = async (e)=>{
+        e.preventDefault()
+        const displayName = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        const file = e.target[3].files[0]; // .files will take input as files and [0] means that only this first file is acceptable
+
+        try{
+            const res =  await createUserWithEmailAndPassword(auth,email,password);
+        }catch(err){
+            setErr(true);
+        }
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            console.log(user);
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+        });
+    }
+
     return(
         <div className="formContainer">
             <div className="formWrapper">
                 <span className="logo">ExoComms</span>
                 <span className="title">Register</span>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input type="text" placeholder="Display Name"/>
                     <input type="email" placeholder="Email"/>
                     <input type="password" placeholder="Password"/>
@@ -17,11 +47,11 @@ const Register = () =>{
                         <span>Add an Avatar</span>
                     </label>
                     <button>Sign Up</button>
+                    {err &&<span>Something went wrong</span>}
                 </form>  
                 <p>You do Have an account ?<a href="Login">Login</a></p>
             </div>
         </div>
     )
 }
-
 export default Register;
